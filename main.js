ba0a1d7ef5ab8ce7588a2c1a927f5f671b0aee29
Cpt.Markizi <Cpt.Markizi@gmail.com>
2023-02-09 17:32:21 +0300

    Второй урок

---
+++ main.js
@@ -0,0 +1,101 @@
+// console.log('Info') 
+// alert('Hello World!')
+// alert('Hello World 2')
+
+// var a;// устаревший вариаент - можем вызвать переменную раньше, чем её обьявили (Больше сложностей?)
+// let b;// современный вариант
+// const c;// неизменяемый вариант
+
+// const myFirstConst = '09.02.2023'; - это неизменяемая переменная
+// myFirstConst = '01.01.2011'
+// console.log(myFirstConst)
+
+// let myFirsConst = '09.02.2023'
+// myFirsConst = '01.01.2011'
+// console.log(myFirsConst);
+
+// let text = 'Text'
+// text = 123
+// console.log(text);+
+
+
+// 1 тип данных числовой (NUMBER)
+
+// console.log(Infinity);
+// console.log(1/0);
+
+// console.log(NaN)
+// console.log('storlka' * 12)
+
+
+// 2 тип данных Bigint
+
+// let a = 9999999999999999999999999999999999n
+// console.log(a)
+// console.log(1)
+// console.log(1)
+
+
+// 3 тип данных (string)
+
+// let str1 = "JOJO1";
+// let str2 = 'JOJO2';
+// let str3 = `JOJO3 ${1+1}`;
+
+// console.log(str1)
+// console.log(str2)
+// console.log(str3)
+
+
+// 4 тип данных - логический тип данных (BOOLean)
+
+// let DotCheck =true
+// let DotUnCheck =false 
+// // let a = 4>1
+// console.log(a)
+
+
+// 5 тип данных NULL
+
+// let nuller = null
+// console.log(nuller)
+
+
+// 6 тип данных undifined
+
+// let test;
+// console.log(test)
+
+
+// 7 тип данных Обьекты
+
+// Разберём позже
+
+
+// 8 тип данных оператор typeof
+
+// typeof(x)
+// typeof x 
+
+// console.log(typeof undefined)
+// console.log(typeof 0)
+// console.log(typeof 1100n)
+// console.log(typeof 'cat')
+// console.log(typeof false)
+// console.log(typeof Symbol ('id'))
+// console.log(typeof null)
+
+
+//document.getElementById('first').innerHTML ='Практическая строка'
+
+//Домашнее задание:
+
+const HomeWork = '29.07.1997';
+console.log(HomeWork)
+
+let text = 'Добро '+'пожаловать'+' на курс,'+ ' Роман'
+console.log (text)
+
+alert('09.02.2023')
+
+document.getElementById('one').innerHTML = 'Hello World'
